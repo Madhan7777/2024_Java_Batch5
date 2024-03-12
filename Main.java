53652f8fc5ae2436d8d6699bcd621a885efa94d5
Madhan7777 <114061615+Madhan7777@users.noreply.github.com>
2024-03-12 09:46:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 class Main
   {
-    int a = 10;
-    int b = 90;
-    int c = a + b;
     public static void main(String[] args) {
-      System.out.print(c);
+      System.out.println("HI");
+      System.out.println("Welcome");
     }
